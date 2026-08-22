ec79cec0f43bbd182f9e6a9f31641872ea8b1828
Roberto <llamparoberto5@gmail.com>
2026-08-22 12:12:09 -0300

    sdads

---
+++ trabajo_practico2/Cilindro.java
@@ -0,0 +1,34 @@
+package trabajo_practico2;
+
+public class Cilindro {
+    private double radio;
+    private double altura;
+
+    public Cilindro(){
+        this.radio = 1.0;
+        this.altura = 1.0;
+    }
+    public Cilindro(double radio,double altura){
+        this.radio = radio;
+        this.altura = altura;
+    }
+
+    public void getAltura(){
+
+    }
+
+    public void setAltura(){
+
+    }
+
+    public void getRadio(){
+
+    }
+    public void setRadio()[
+        
+    ]
+
+    public double calcular_volumen(double radio, double altura){
+
+    }
+}
